0bf733244a34685d9807f8044d456d2d099e29bf
ABDELRAHMAN ABDELMONEIM (Bedo) <Abdelrahman.Abdelmoniem@gmail.com>
2019-07-18 23:01:32 -0400

    Create EvenOrOdd.cpp

---
+++ EvenOrOdd.cpp
@@ -0,0 +1,14 @@
+//Check whether a Number is Even or Odd using if else
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout <<"Please enter a number"<<endl;
+    cin >> x;
+    if (x % 2 == 0){
+        cout<<"That's an even number"<<endl;
+    }else{
+        cout<<"That's and odd number"<<endl;
+    }
+
+}
