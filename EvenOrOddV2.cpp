62b250c61240f4136ab47086575161310026a221
ABDELRAHMAN ABDELMONEIM (Bedo) <Abdelrahman.Abdelmoniem@gmail.com>
2019-07-18 23:13:49 -0400

    Creat EvenOrOddv2.cpp

---
+++ EvenOrOddV2.cpp
@@ -0,0 +1,11 @@
+//Check whether Number is Even or Odd using ternary operators
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout <<"Please enter a number"<<endl;
+    cin >> x;
+    
+     (x%2==0) ? cout<<"That's an even number"<<endl : cout<<"That's and odd number"<<endl;
+
+}
